54da68883f072a9f8807ea02f50a39944d1e598a
Anselm Baird-Smith <abairdsmith@gmail.com>
2022-12-05 18:05:17 +0100

    Removed some empty lines.

--- app/src/main/java/com/anselm/books/ui/home/HomeFragment.kt
+++ app/src/main/java/com/anselm/books/ui/home/HomeFragment.kt
@@ -21,11 +21,7 @@ import com.anselm.books.databinding.FragmentHomeBinding
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
