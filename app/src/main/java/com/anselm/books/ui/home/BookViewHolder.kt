d215b0760383d332763eb74c9460db9384d816de
anselm <abairdsmith@gmail.com>
2023-01-23 23:34:05 +0100

    Added subtitle to the list view.

--- app/src/main/java/com/anselm/books/ui/home/BookViewHolder.kt
+++ app/src/main/java/com/anselm/books/ui/home/BookViewHolder.kt
@@ -3,10 +3,10 @@ package com.anselm.books.ui.home
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
-import com.anselm.books.database.Book
 import com.anselm.books.BooksApplication
 import com.anselm.books.GlideApp
 import com.anselm.books.R
+import com.anselm.books.database.Book
 import com.anselm.books.databinding.RecyclerviewBookItemBinding
 
 class BookViewHolder(
@@ -21,6 +21,12 @@ class BookViewHolder(
         val uri = app.imageRepository.getCoverUri(book)
         binding.titleView.text = book.title
         binding.authorView.text = book.authors.joinToString { it.name }
+        if (book.subtitle.isEmpty()) {
+            binding.subtitleView.isVisible = false
+        } else {
+            binding.subtitleView.isVisible = true
+            binding.subtitleView.text = book.subtitle
+        }
         if (book.dateAdded != "") {
             binding.dateAddedView.text = app.getString(R.string.date_added_embedded, book.dateAdded)
         }
