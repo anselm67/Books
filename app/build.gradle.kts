7dd338f50e1bfb49ae574ed9f68bd066146eceac
anselm <abairdsmith@gmail.com>
2024-12-29 17:37:54 +0100

    Migrated to .kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,103 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.navigation.safeargs.kotlin)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.com.google.devtools.ksp)
+}
+
+android {
+    namespace = "com.anselm.books"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.anselm.books"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        applicationId = "com.anselm.books"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.ktx)
+
+    // Room components
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.heifwriter)
+
+    implementation(libs.androidx.paging.runtime.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Lifecycle components
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.common.java8)
+
+    // UI
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
+
+    // Preferences
+    implementation(libs.androidx.preference.ktx)
+
+    // Glide
+    implementation(libs.glide)
+    ksp(libs.glide.compiler)
+
+    // Kotlin reflection
+    implementation(libs.kotlin.reflect)
+
+    // OkHttp for OpenLibrary and GoogleBooks access.
+    implementation(libs.okhttp)
+
+    // CameraX
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.video)
+
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
+
+    // Barcode scanning with mlkit, including the model.
+    implementation(libs.barcode.scanning)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.core.testing)
+    androidTestImplementation(libs.androidx.junit)
+}
